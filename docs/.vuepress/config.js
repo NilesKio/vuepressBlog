991bf9d90d5463f89bf9ebd043c256d6b6d994ad
Niles <l13911963456@icloud.com>
2020-06-18 14:55:39 +0800

    my-vlog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,18 +17,13 @@ serviceWorker: true,
   },
 themeConfig: {
         // 你的GitHub仓库，请正确填写
-        repo: 'https://github.com/NilesKio/vuepressBlog',
+        repo: 'https://github.com/NilesKio/vuepressBlog.git',
         // 自定义仓库链接文字。
         repoLabel: 'My GitHub',
         nav: [
             { text: 'Home', link: '/' },
             { text: '第一篇博客', link: '/blog/haha1.md' }
-        ],
-
-sidebar: [
-      ['/', '首页'],
-      ['/blog/haha1.md', '我的第一篇博客']
-    ]
+        ]
 
 
 
