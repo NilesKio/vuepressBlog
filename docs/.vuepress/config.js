5480e4d7cd6388838994b2529f2f3b36ab981843
Niles <l13911963456@icloud.com>
2020-06-18 14:45:46 +0800

    my-vlog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,39 +5,24 @@ module.exports = {
     ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
 
  ['link', { rel: 'manifest', href: '/photo.jpg' }],
-  ['link', { rel: 'apple-touch-icon', href: '/photo.jpg' }],
-
-
+  ['link', { rel: 'apple-touch-icon', href: '/photo.jpg' }]
 
   ],
 
 serviceWorker: true,
 
- themeConfig: {
-        // 你的GitHub仓库，请正确填写
-        repo: 'https://github.com/NilesKio/vuepressBlog',
-        // 自定义仓库链接文字。
-        repoLabel: 'My GitHub',
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: '搭建心得', link: '/blog/haha1.md' }
-        ]
-
-  sidebar: [
-      ['/', '首页'],
-      ['/blog/haha1.md', '搭建心得']
-    ]
-
-​    }
-
-
-
-
-
-
-
   base: '/', // 这是部署到github相关的配置
   markdown: {
     lineNumbers: false // 代码块显示行号
   },
+themeConfig: {
+        // 你的GitHub仓库，请正确填写
+        repo: 'https://github.com/xxxxxxx/blog-demo',
+        // 自定义仓库链接文字。
+        repoLabel: 'My GitHub',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: '第一篇博客', link: '/blog/haha1.md' }
+        ]
+    }
 };
